3871568bf2f3228c4e88caa732bf3814db0944c5
Alex "Memecian" Komyakov <ememecian@gmail.com>
2020-10-05 18:57:08 +0200

    05102020 - Commit 1
    ## MPU
    - Added UART functions, god knows if they work

--- MPU/MPU/main.c
+++ MPU/MPU/main.c
@@ -4,14 +4,38 @@
  * Author : Administrator
  */ 
 
+#define F_CPU 8000000UL
+
+#define set_bit(var, bit) ((var) |= (1 << (bit)))
+#define clear_bit(var, bit) ((var) &= (unsigned)~(1 << (bit)))
+#define bit_is_set(var, bit) ((var) & (1 << (bit)))
+#define bit_is_clear(var, bit) !bit_is_set(var, bit)
+
 #include <avr/io.h>
-#include "../"
+#include <util/delay.h>
+
+void uartInit(void)
+{
+	set_bit(UCSR0B, RXEN0);	// Enable UART transmitter mode
+	set_bit(UCSR0C, UMSEL00);	// Register select
+	set_bit(UCSR0C, UCSZ00);
+	set_bit(UCSR0C, UCSZ01);	// Select 8-bit packet mode
+	UBRR0L = 51;				// Set baud rate (9600)
+}
+
+void uartTransmit(char *p)
+{
+	while(*p)
+	{
+		UDR0 = (*(p++));
+		while(bit_is_set(UCSR0A, TXC0));
+	}
+}
 
 int main(void)
 {
-    /* Replace with your application code */
+    
     while (1) 
     {
     }
 }
-
