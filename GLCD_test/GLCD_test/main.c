2952a4cd4d5f941ae5e531c5634377db54a698f6
Alex "Memecian" Komyakov <ememecian@gmail.com>
2020-12-02 11:48:14 +0100

    02122020 - Commit I

--- GLCD_test/GLCD_test/main.c
+++ GLCD_test/GLCD_test/main.c
@@ -1,18 +1,16 @@
 /*
  * GLCD_test.c
- *
  * Created: 19.10.2020 11:51:02
  * Author : Administrator
  */ 
 
 #include <avr/io.h>
-
+#include "../../alko_glcd/alko_glcd/glcd.c"
 
 int main(void)
 {
-    /* Replace with your application code */
     while (1) 
     {
-    }
+		_glcdDebugFill();
+	}
 }
-
